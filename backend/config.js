efe9cdf20977268d39bcec84a32dcc263dd6449f
tmrich212 <57809945+tmrich212@users.noreply.github.com>
2023-09-25 20:01:36 -0400

    DB connected w/ Mongoose, test with Insomnia

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBUrl = "mongodb+srv://mybookstore:bookstoremern23@book-store-mern.ivozora.mongodb.net/books-collection?retryWrites=true&w=majority";
+
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,70 @@
-import express from 'express';
-import { PORT } from './config.js';
+import express from "express";
+import mongoose from "mongoose";
+import { PORT, mongoDBUrl } from "./config.js";
+import { Book } from "./models/bookModels.js";
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`)
+//middleware needed to allow express to use json - parsing request body
+app.use(express.json())
+
+//we need a route to use GET for server
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Welcome to MERN Stack Bookstore!");
+});
+
+//route to save new book - this is asynchronous from working with mongoose
+app.post("/books", async (request, response) => {
+  try {
+   if(!request.body.title ||
+    !request.body.author ||
+    !request.body.publishYear){
+        return response.status(400).send({ message: "Send all required fields: title, author, publishYear" })
+    }
+    //create variable for newBook
+    const newBook = {
+        title: request.body.title,
+        author: request.body.author,
+        publishYear: request.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+    //send new book to client
+    return response.status(201).send(book);
+
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+//route for Get All Books from DB 
+app.get('/books', async (request, response) => {
+    try {
+        const books = await Book.find({});
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
 })
+
+//use mongoose to connect to DB
+mongoose
+  .connect(mongoDBUrl)
+  .then(() => {
+    console.log("App connected to database");
+    //listen on static port
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+//mongoose object data library model for mongoDB
